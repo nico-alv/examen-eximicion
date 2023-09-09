2e90ea533260904b7cf03ec0840ed421ae1f4121
nico-alv <nicolas.alvarez02@alumnos.ucn.cl>
2023-09-09 18:57:57 -0300

    fix: error de cors

--- proyecto-examen-vue/src/views/users/UserCreate.vue
+++ proyecto-examen-vue/src/views/users/UserCreate.vue
@@ -4,8 +4,8 @@
     const { storeUser, errors } = useUsers();
 
     const form = reactive({
-        name: "",
-        slug: ""
+        rut: "",
+        email : ""
     })
 </script>
 <template>
@@ -13,17 +13,17 @@
     <form class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md" @submit.prevent="storeUser(form)">
         <div class="space-y-6">
             <div class="mb-6">
-                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Name</label>
-                <input type="text" id="name" v-model="form.rut" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <div v-if="errors.name">
+                <label for="rut" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">RUT</label>
+                <input type="text" id="rut" v-model="form.rut" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <div v-if="errors.rut">
                     <span class="text-sm text-red-400">{{ errors.name[0] }}</span>
                 </div>
             </div>
             <div class="mb-6">
-                <label for="slug" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Slug</label>
-                <input type="text" id="slug" v-model="form.name" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <div v-if="errors.slug">
-                    <span class="text-sm text-red-400">{{ errors.slug[0] }}</span>
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Name</label>
+                <input type="text" id="name" v-model="form.name" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <div v-if="errors.name">
+                    <span class="text-sm text-red-400">{{ errors.name[0] }}</span>
                 </div>
             </div>
         </div>
